c872770c366080c828cc981d2aa35e08f3cd9c72
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-21 20:52:38 +0900

    ok

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Todo from './todo';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo />
   </React.StrictMode>
 );
 
