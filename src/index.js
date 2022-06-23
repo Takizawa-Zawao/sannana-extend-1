7b7c0dac7fd5c4c9d970fdf8fc6a1a07ed4aad8e
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-23 16:23:12 +0900

    ok

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Component from "react";
+
+export default class TestApp extends Component{
+    render(){
+        return <h1>React.</h1>
+    }
+}
+
+const app = document.getElementById("app");
+ReactDom.render(<TestApp />, app);
