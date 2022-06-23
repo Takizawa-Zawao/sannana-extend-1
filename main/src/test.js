6d1acce1b2c9d5f51d1eaaa075ed289608fcec6c
Takizawa-Zawao <106427546+Takizawa-Zawao@users.noreply.github.com>
2022-06-23 19:55:41 +0900

    ok

--- main/src/test.js
+++ main/src/test.js
@@ -4,7 +4,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Component from "react";
 */
-export default class TestApp extends Component{
+export default class TestApp extends React.Component{
     render(){
         return <h1>React.</h1>;
     };
